5530bd13b0bc59764e546e61ff01a96a1c2ea3c4
Lee Reilly <lee@leereilly.net>
2017-04-21 18:59:55 -0700

    We bouncin'

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,9 +1,36 @@
 var game = {};
 
 game.create = function () {
-  var player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
+  // use arcade physics
+  this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  // introduce our hero
+  player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
   player.anchor.setTo(0.5, 0.5);
-  player.scale.setTo(600,600);
+  player.scale.setTo(300,300);
+  this.game.physics.arcade.enable(player);
+  player.enableBody = true;
+  player.body.bounce.setTo(0.9, 0.9);
+  player.body.collideWorldBounds=true;
+
+  // enable cursor keys
+  cursors = this.input.keyboard.createCursorKeys();
 };
+
+game.update = function () {
+  if (cursors.up.isDown) {
+      this.game.physics.arcade.accelerationFromRotation(player.rotation, 300, player.body.acceleration);
+  } else {
+      player.body.acceleration.set(0);
+  }
+
+  if (cursors.left.isDown) {
+      player.body.angularVelocity = -300;
+  } else if (cursors.right.isDown) {
+      player.body.angularVelocity = 300;
+  } else {
+      player.body.angularVelocity = 0;
+  }
+}
 
 module.exports = game;
